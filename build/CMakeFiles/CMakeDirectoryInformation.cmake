984f85b4b68ae6b43cd3e2212e7ae97793979224
riya-sik <riya.s@avench.com>
2024-10-28 16:26:05 +0530

    final version v1.0

--- build/CMakeFiles/CMakeDirectoryInformation.cmake
+++ build/CMakeFiles/CMakeDirectoryInformation.cmake
@@ -3,7 +3,7 @@
 
 # Relative path conversion top directories.
 set(CMAKE_RELATIVE_PATH_TOP_SOURCE "/home/riya/demo4")
-set(CMAKE_RELATIVE_PATH_TOP_BINARY "/home/riya/demo4")
+set(CMAKE_RELATIVE_PATH_TOP_BINARY "/home/riya/demo4/build")
 
 # Force unix paths in dependencies.
 set(CMAKE_FORCE_UNIX_PATHS 1)
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/riya/demo4/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/riya/demo4/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
